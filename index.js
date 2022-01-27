10194d95414b0eb9d223866e8c726d5b606794b6
Lobotomized <ivan_dzholev@abv.bg>
2022-01-27 13:17:45 +0200

    visual fixes

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('./routes/test.js')(app)
 require('./routes/auth.js')(app)
-app.use('/', express.static(path.join(__dirname, 'public')))
+// app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 server.listen(8080)
